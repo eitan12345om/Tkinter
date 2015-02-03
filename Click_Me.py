bf1d87ce387df37d3a5b81bdadc4c676014ffd13
eitan12345om <eitan.simler@gmail.com>
2015-02-02 17:21:34 -0800

    Initial Commit

---
+++ Click_Me.py
@@ -0,0 +1,48 @@
+from tkinter import *
+import random
+
+root = Tk()
+root.title('Button Clicker')
+
+counter = IntVar()
+counter.set(0)
+
+counter_string = StringVar()
+counter_string.set("You've clicked the button %d times" % counter.get())
+
+def add_click():
+	counter.set(counter.get() + 1)
+	if counter.get() == 1:
+		counter_string.set("You've clicked the button %d time" % counter.get())
+	else:
+		counter_string.set("You've clicked the button %d times" % counter.get())
+def reset_clicks():
+	counter.set(0)
+	counter_string.set("You've clicked the button %d times" % counter.get())
+
+
+label = Label(root,
+	textvariable = counter_string
+)
+label.pack(side = TOP)
+
+button = Button(root,
+	text = "Click Me!",
+	font = ("Helvetica", 16, "bold"),
+	padx = 5,
+	command = add_click
+)
+button.pack(side = LEFT)
+
+RESET = Button(root,
+	text = "Reset",
+	bg = "red",
+	font = ("Helvetica", 16, "bold"),
+	padx = 5,
+	command = reset_clicks
+)
+
+
+RESET.pack(side = RIGHT)
+
+root.mainloop()
