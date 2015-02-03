a6ae35a943b8529c52b839c6c7f2168bc414e9c4
eitan12345om <eitan.simler@gmail.com>
2015-02-02 17:21:11 -0800

    Added power to the equal sign

--- Calculator.py
+++ Calculator.py
@@ -19,13 +19,18 @@ class Calculator:
 	def divide(self, a, b):
 		return round(a / b, 5)
 
+class CalcButton(Button):
+	def __init__(self, master, number, callback):
+		button_args = dict(width = 4, height = 2, bg = 'light blue', font = ("Helvetica", 10, "bold"))
+		super().__init__(master, text = number, command = lambda: callback(number), **button_args)
+
 class Interface:
 	def __init__(self, master):
+		self.Calc = Calculator()
+
 		self.display_maxLength = 10
 		self.display_numbers = StringVar()
 		self.small_display_numbers = StringVar()
-
-		number_button_params = dict(width = 4, height = 2, bg = 'light blue', font = ("Helvetica", 10, "bold"))	
 
 		# Create display
 		small_display = Message(master, textvariable = self.small_display_numbers, font = ("Times", 10), width = 200, bg = 'white', anchor = E)
@@ -34,16 +39,16 @@ class Interface:
 		display.grid(column = 0, row = 1, columnspan = 4, sticky = NSEW)
 
 		# number buttons
-		number1 = Button(master, text = "1", command = lambda: self.show_number('1'), **number_button_params)
-		number2 = Button(master, text = "2", command = lambda: self.show_number('2'), **number_button_params)
-		number3 = Button(master, text = "3", command = lambda: self.show_number('3'), **number_button_params)
-		number4 = Button(master, text = "4", command = lambda: self.show_number('4'), **number_button_params)
-		number5 = Button(master, text = "5", command = lambda: self.show_number('5'), **number_button_params)
-		number6 = Button(master, text = "6", command = lambda: self.show_number('6'), **number_button_params)
-		number7 = Button(master, text = "7", command = lambda: self.show_number('7'), **number_button_params)
-		number8 = Button(master, text = "8", command = lambda: self.show_number('8'), **number_button_params)
-		number9 = Button(master, text = "9", command = lambda: self.show_number('9'), **number_button_params)
-		number0 = Button(master, text = "0", command = lambda: self.show_number('0'), **number_button_params)
+		number1 = CalcButton(master, "1", self.show_number)
+		number2 = CalcButton(master, "2", self.show_number)
+		number3 = CalcButton(master, "3", self.show_number)
+		number4 = CalcButton(master, "4", self.show_number)
+		number5 = CalcButton(master, "5", self.show_number)
+		number6 = CalcButton(master, "6", self.show_number)
+		number7 = CalcButton(master, "7", self.show_number)
+		number8 = CalcButton(master, "8", self.show_number)
+		number9 = CalcButton(master, "9", self.show_number)
+		number0 = CalcButton(master, "0", self.show_number)
 		number1.grid(column = 0, row = 3, padx = 3, pady = 3)
 		number2.grid(column = 1, row = 3, padx = 3, pady = 3)
 		number3.grid(column = 2, row = 3, padx = 3, pady = 3)
@@ -62,22 +67,21 @@ class Interface:
 		clear_display.grid(column = 1, row = 2, padx = 3, pady = 3)
 		clear_all = Button(master, text = "C", width = 4, height = 2, bg = 'light blue', font = ("Helvetica", 10, "bold"), command = self.clear_interface)
 		clear_all.grid(column = 2, row = 2, padx = 3, pady = 3)
-		
 
-		# signs of operation
+		# signs of operation and decimal
 		plus = Button(master, text = "+", width = 4, height = 2, bg = 'light blue', font = ("Helvetica", 10, "bold"), command = lambda: self.small_interface_shift('+'))
-		plus.grid(column = 3, row = 3, padx = 3, pady = 3)
+		plus.grid(column = 3, row = 2, padx = 3, pady = 3)
 		minus = Button(master, text = "-", width = 4, height = 2, bg = 'light blue', font = ("Helvetica", 10, "bold"), command = lambda: self.small_interface_shift('-'))
-		minus.grid(column = 3, row = 4, padx = 3, pady = 3)
+		minus.grid(column = 3, row = 3, padx = 3, pady = 3)
 		multiply = Button(master, text = "*", width = 4, height = 2, bg = 'light blue', font = ("Helvetica", 10, "bold"), command = lambda: self.small_interface_shift('*'))
-		multiply.grid(column = 3, row = 5, padx = 3, pady = 3)
+		multiply.grid(column = 3, row = 4, padx = 3, pady = 3)
 		divide = Button(master, text = "/", width = 4, height = 2, bg = 'light blue', font = ("Helvetica", 10, "bold"), command = lambda: self.small_interface_shift('/'))
-		divide.grid(column = 3, row = 6, padx = 3, pady = 3)
+		divide.grid(column = 3, row = 5, padx = 3, pady = 3)
 		equal = Button(master, text = "=", width = 4, height = 2, bg = 'light blue', font = ("Helvetica", 10, "bold"), command = self.press_equal)
-		equal.grid(column = 1, row = 6, padx = 3, pady = 3, columnspan = 2, sticky = EW)
-		
-		print_to_screen = Button(master, text = "prs", width = 4, height = 2, bg = 'light blue', font = ("Helvetica", 10, "bold"), command = self.prs)
-		print_to_screen.grid(column = 3, row = 2, columnspan = 2, padx = 3, pady = 3, sticky = EW)
+		equal.bind("<Return>", self.press_equal())
+		equal.grid(column = 2, row = 6, padx = 3, pady = 3, columnspan = 2, sticky = EW)
+		decimal = Button(master, text = ".", width = 4, height = 2, bg = 'light blue', font = ("Helvetica", 10, "bold"), command = self.add_decimal)
+		decimal.grid(column = 1, row = 6, padx = 3, pady = 3)
 
 	def show_number(self, number):
 		if len(self.display_numbers.get()) < self.display_maxLength:
@@ -97,9 +101,12 @@ class Interface:
 		self.clear_disp()
 		self.small_display_numbers.set('')
 
-	def prs(self):
-		if len(self.display_numbers.get()) > 0:
-			print(int(self.display_numbers.get()))
+	def add_decimal(self):
+		if '.' not in self.display_numbers.get():
+			if len(self.display_numbers.get()) == 0:
+				self.display_numbers.set('0.')
+			else:
+				self.display_numbers.set(self.display_numbers.get() + '.')
 
 	def small_interface_shift(self, operator):
 		if len(self.small_display_numbers.get()) == 0:
@@ -111,8 +118,19 @@ class Interface:
 
 	def press_equal(self):
 		if len(self.small_display_numbers.get()) > 0 and len(self.display_numbers.get()):
-			print('Hi')
+			if self.small_display_numbers.get()[-1] == '+':
+				answer = str(self.Calc.add(float(self.small_display_numbers.get()[:-2]), float(self.display_numbers.get())))
+			elif self.small_display_numbers.get()[-1] == '-':
+				answer = str(self.Calc.subtract(float(self.small_display_numbers.get()[:-2]), float(self.display_numbers.get())))
+			elif self.small_display_numbers.get()[-1] == '*':
+				answer = str(self.Calc.multiply(float(self.small_display_numbers.get()[:-2]), float(self.display_numbers.get())))
+			else:
+				answer = str(self.Calc.divide(float(self.small_display_numbers.get()[:-2]), float(self.display_numbers.get())))
 
+			self.clear_interface()
+			if answer[-2:] == '.0':
+				answer = answer[:-2]
+			self.display_numbers.set(answer)
 
 GUI = Interface(root)
 root.mainloop()
