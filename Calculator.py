a229cad3ab5ba3916afae490bb61df2764c18412
eitan12345om <eitan.simler@gmail.com>
2015-02-04 21:13:07 -0800

    Organised a bit more. Work on a few functions

--- Calculator.py
+++ Calculator.py
@@ -29,7 +29,7 @@ class Calculator:
 
 class CalcButton(Button):
 	def __init__(self, master, number, callback):
-		button_args = dict(width = 4, height = 2, bg = 'light blue', font = ("Helvetica", 10, "bold"), highlightcolor = 'gold')
+		button_args = dict(width = 4, height = 2, bg = 'PaleTurquoise', font = ("Helvetica", 10, "bold"), highlightcolor = 'gold')
 		super().__init__(master, text = number, command = lambda: callback(number), **button_args)
 
 class Interface:
@@ -52,6 +52,7 @@ class Interface:
 		master.bind('*', lambda event: self.small_interface_shift('*'))
 		master.bind('/', lambda event: self.small_interface_shift('/'))
 		master.bind('.', self.add_decimal)
+		master.bind('^', lambda event: self.small_interface_shift('^'))
 
 		self.Calc = Calculator()
 
@@ -60,9 +61,9 @@ class Interface:
 		self.small_display_numbers = StringVar()
 
 		# Create display
-		small_display = Message(master, textvariable = self.small_display_numbers, font = ("Times", 10), width = 200, bg = 'white', anchor = E)
+		small_display = Message(master, textvariable = self.small_display_numbers, font = ("Times", 10), width = 220, bg = 'white', anchor = E)
 		small_display.grid(column = 0, row = 0, columnspan = 5, sticky = NSEW)
-		display = Message(master, textvariable = self.display_numbers, font = ("Helvetica", 16, "bold"), width = 200, bg = 'white', anchor = E)
+		display = Message(master, textvariable = self.display_numbers, font = ("Helvetica", 16, "bold"), width = 220, bg = 'white', anchor = E)
 		display.grid(column = 0, row = 1, columnspan = 5, sticky = NSEW)
 
 		# number buttons
@@ -76,16 +77,16 @@ class Interface:
 		number8 = CalcButton(master, "8", self.show_number)
 		number9 = CalcButton(master, "9", self.show_number)
 		number0 = CalcButton(master, "0", self.show_number)
-		number1.grid(column = 0, row = 3, padx = 3, pady = 3)
-		number2.grid(column = 1, row = 3, padx = 3, pady = 3)
-		number3.grid(column = 2, row = 3, padx = 3, pady = 3)
+		number1.grid(column = 0, row = 5, padx = 3, pady = 3)
+		number2.grid(column = 1, row = 5, padx = 3, pady = 3)
+		number3.grid(column = 2, row = 5, padx = 3, pady = 3)
 		number4.grid(column = 0, row = 4, padx = 3, pady = 3)
 		number5.grid(column = 1, row = 4, padx = 3, pady = 3)
 		number6.grid(column = 2, row = 4, padx = 3, pady = 3)
-		number7.grid(column = 0, row = 5, padx = 3, pady = 3)
-		number8.grid(column = 1, row = 5, padx = 3, pady = 3)
-		number9.grid(column = 2, row = 5, padx = 3, pady = 3)
-		number0.grid(column = 0, row = 6, padx = 3, pady = 3)
+		number7.grid(column = 0, row = 3, padx = 3, pady = 3)
+		number8.grid(column = 1, row = 3, padx = 3, pady = 3)
+		number9.grid(column = 2, row = 3, padx = 3, pady = 3)
+		number0.grid(column = 0, row = 6, padx = 3, pady = 3 ,columnspan = 2, sticky = EW)
 		number1.bind("<Enter>", lambda event: number1.config(bg = "gold"))
 		number2.bind("<Enter>", lambda event: number2.config(bg = "gold"))
 		number3.bind("<Enter>", lambda event: number3.config(bg = "gold"))
@@ -96,69 +97,82 @@ class Interface:
 		number8.bind("<Enter>", lambda event: number8.config(bg = "gold"))
 		number9.bind("<Enter>", lambda event: number9.config(bg = "gold"))
 		number0.bind("<Enter>", lambda event: number0.config(bg = "gold"))
-		number1.bind("<Leave>", lambda event: number1.config(bg = "light blue"))
-		number2.bind("<Leave>", lambda event: number2.config(bg = "light blue"))
-		number3.bind("<Leave>", lambda event: number3.config(bg = "light blue"))
-		number4.bind("<Leave>", lambda event: number4.config(bg = "light blue"))
-		number5.bind("<Leave>", lambda event: number5.config(bg = "light blue"))
-		number6.bind("<Leave>", lambda event: number6.config(bg = "light blue"))
-		number7.bind("<Leave>", lambda event: number7.config(bg = "light blue"))
-		number8.bind("<Leave>", lambda event: number8.config(bg = "light blue"))
-		number9.bind("<Leave>", lambda event: number9.config(bg = "light blue"))
-		number0.bind("<Leave>", lambda event: number0.config(bg = "light blue"))
+		number1.bind("<Leave>", lambda event: number1.config(bg = "PaleTurquoise"))
+		number2.bind("<Leave>", lambda event: number2.config(bg = "PaleTurquoise"))
+		number3.bind("<Leave>", lambda event: number3.config(bg = "PaleTurquoise"))
+		number4.bind("<Leave>", lambda event: number4.config(bg = "PaleTurquoise"))
+		number5.bind("<Leave>", lambda event: number5.config(bg = "PaleTurquoise"))
+		number6.bind("<Leave>", lambda event: number6.config(bg = "PaleTurquoise"))
+		number7.bind("<Leave>", lambda event: number7.config(bg = "PaleTurquoise"))
+		number8.bind("<Leave>", lambda event: number8.config(bg = "PaleTurquoise"))
+		number9.bind("<Leave>", lambda event: number9.config(bg = "PaleTurquoise"))
+		number0.bind("<Leave>", lambda event: number0.config(bg = "PaleTurquoise"))
 
 		# backspace and clear
-		backspace = Button(master, text = "←", width = 4, height = 2, bg = 'light blue', font = ("Helvetica", 10, "bold"), command = self.backspace)
-		clear_display = Button(master, text = "CE", width = 4, height = 2, bg = 'light blue', font = ("Helvetica", 10, "bold"), command = self.clear_disp)
-		clear_all = Button(master, text = "C", width = 4, height = 2, bg = 'light blue', font = ("Helvetica", 10, "bold"), command = self.clear_interface)
+		backspace = Button(master, text = "←", width = 4, height = 2, bg = 'LightSteelBlue', font = ("Helvetica", 10, "bold"), command = self.backspace)
+		clear_display = Button(master, text = "CE", width = 4, height = 2, bg = 'LightSteelBlue', font = ("Helvetica", 10, "bold"), command = self.clear_disp)
+		clear_all = Button(master, text = "C", width = 4, height = 2, bg = 'LightSteelBlue', font = ("Helvetica", 10, "bold"), command = self.clear_interface)
 		backspace.grid(column = 0, row = 2, padx = 3, pady = 3)
 		clear_display.grid(column = 1, row = 2, padx = 3, pady = 3)
 		clear_all.grid(column = 2, row = 2, padx = 3, pady = 3)
 		backspace.bind("<Enter>", lambda event: backspace.config(bg = "gold"))
 		clear_display.bind("<Enter>", lambda event: clear_display.config(bg = "gold"))
 		clear_all.bind("<Enter>", lambda event: clear_all.config(bg = "gold"))
-		backspace.bind("<Leave>", lambda event: backspace.config(bg = "light blue"))
-		clear_display.bind("<Leave>", lambda event: clear_display.config(bg = "light blue"))
-		clear_all.bind("<Leave>", lambda event: clear_all.config(bg = "light blue"))
+		backspace.bind("<Leave>", lambda event: backspace.config(bg = "LightSteelBlue"))
+		clear_display.bind("<Leave>", lambda event: clear_display.config(bg = "LightSteelBlue"))
+		clear_all.bind("<Leave>", lambda event: clear_all.config(bg = "LightSteelBlue"))
 
 		# signs of operation and decimal
-		plus = Button(master, text = "+", width = 4, height = 2, bg = 'light blue', font = ("Helvetica", 10, "bold"), command = lambda: self.small_interface_shift('+'))
-		minus = Button(master, text = "-", width = 4, height = 2, bg = 'light blue', font = ("Helvetica", 10, "bold"), command = lambda: self.small_interface_shift('-'))
-		multiply = Button(master, text = "*", width = 4, height = 2, bg = 'light blue', font = ("Helvetica", 10, "bold"), command = lambda: self.small_interface_shift('*'))
-		divide = Button(master, text = "/", width = 4, height = 2, bg = 'light blue', font = ("Helvetica", 10, "bold"), command = lambda: self.small_interface_shift('/'))
-		equal = Button(master, text = "=", width = 4, height = 2, bg = 'light blue', font = ("Helvetica", 10, "bold"), command = self.press_equal)
-		decimal = Button(master, text = ".", width = 4, height = 2, bg = 'light blue', font = ("Helvetica", 10, "bold"), command = self.add_decimal)
-		plus.grid(column = 3, row = 2, padx = 3, pady = 3)
-		minus.grid(column = 3, row = 3, padx = 3, pady = 3)
+		plus = Button(master, text = "+", width = 4, height = 2, bg = 'LightSteelBlue', font = ("Helvetica", 10, "bold"), command = lambda: self.small_interface_shift('+'))
+		minus = Button(master, text = "-", width = 4, height = 2, bg = 'LightSteelBlue', font = ("Helvetica", 10, "bold"), command = lambda: self.small_interface_shift('-'))
+		multiply = Button(master, text = "*", width = 4, height = 2, bg = 'LightSteelBlue', font = ("Helvetica", 10, "bold"), command = lambda: self.small_interface_shift('*'))
+		divide = Button(master, text = "/", width = 4, height = 2, bg = 'LightSteelBlue', font = ("Helvetica", 10, "bold"), command = lambda: self.small_interface_shift('/'))
+		equal = Button(master, text = "=", width = 4, height = 2, bg = 'LightSteelBlue', font = ("Helvetica", 10, "bold"), command = self.press_equal)
+		decimal = Button(master, text = ".", width = 4, height = 2, bg = 'LightSteelBlue', font = ("Helvetica", 10, "bold"), command = self.add_decimal)
+		negatify = Button(master, text = "±", width = 4, height = 2, bg = 'LightSteelBlue', font = ("Helvetica", 10, "bold"), command = self.make_negative)
+		plus.grid(column = 3, row = 6, padx = 3, pady = 3)
+		minus.grid(column = 3, row = 5, padx = 3, pady = 3)
 		multiply.grid(column = 3, row = 4, padx = 3, pady = 3)
-		divide.grid(column = 3, row = 5, padx = 3, pady = 3)
-		equal.grid(column = 2, row = 6, padx = 3, pady = 3, columnspan = 2, sticky = EW)
-		decimal.grid(column = 1, row = 6, padx = 3, pady = 3)
+		divide.grid(column = 3, row = 3, padx = 3, pady = 3)
+		equal.grid(column = 4, row = 5, padx = 3, pady = 3, rowspan = 2, sticky = NS)
+		decimal.grid(column = 2, row = 6, padx = 3, pady = 3)
+		negatify.grid(column = 3, row = 2, padx = 3, pady = 3)
 		plus.bind("<Enter>", lambda event: plus.config(bg = 'gold'))
 		minus.bind("<Enter>", lambda event: minus.config(bg = 'gold'))
 		multiply.bind("<Enter>", lambda event: multiply.config(bg = 'gold'))
 		divide.bind("<Enter>", lambda event: divide.config(bg = 'gold'))
 		equal.bind("<Enter>", lambda event: equal.config(bg = 'gold'))
 		decimal.bind("<Enter>", lambda event: decimal.config(bg = 'gold'))
-		plus.bind("<Leave>", lambda event: plus.config(bg = "light blue"))
-		minus.bind("<Leave>", lambda event: minus.config(bg = "light blue"))
-		multiply.bind("<Leave>", lambda event: multiply.config(bg = "light blue"))
-		divide.bind("<Leave>", lambda event: divide.config(bg = "light blue"))
-		equal.bind("<Leave>", lambda event: equal.config(bg = "light blue"))
-		decimal.bind("<Leave>", lambda event: decimal.config(bg = "light blue"))
+		negatify.bind("<Enter>", lambda event: negatify.config(bg = 'gold'))
+		plus.bind("<Leave>", lambda event: plus.config(bg = "LightSteelBlue"))
+		minus.bind("<Leave>", lambda event: minus.config(bg = "LightSteelBlue"))
+		multiply.bind("<Leave>", lambda event: multiply.config(bg = "LightSteelBlue"))
+		divide.bind("<Leave>", lambda event: divide.config(bg = "LightSteelBlue"))
+		equal.bind("<Leave>", lambda event: equal.config(bg = "LightSteelBlue"))
+		decimal.bind("<Leave>", lambda event: decimal.config(bg = "LightSteelBlue"))
+		negatify.bind("<Leave>", lambda event: negatify.config(bg = 'LightSteelBlue'))
 
 		# more advanced operators
-		square = Button(master, text = "x²", width = 4, height = 2, bg = 'light blue', font = ("Helvetica", 10, "bold"))
-		square.grid(column = 4, row = 2, padx = 3, pady = 3)
-		exponent = Button(master, text = "xʸ", width = 4, height = 2, bg = 'light blue', font = ("Helvetica", 10, "bold"), command = lambda: self.small_interface_shift('^'))
+		square = Button(master, text = "x²", width = 4, height = 2, bg = 'LightSteelBlue', font = ("Helvetica", 10, "bold"), command = lambda: self.press_exponentiate(2))
+		exponent = Button(master, text = "xʸ", width = 4, height = 2, bg = 'LightSteelBlue', font = ("Helvetica", 10, "bold"), command = lambda: self.small_interface_shift('^'))
+		squareroot = Button(master, text = "√x", width = 4, height = 2, bg = 'LightSteelBlue', font = ("Helvetica", 10, "bold"), command = lambda: self.press_exponentiate(0.5))
+		some_root = Button(master, text = "ʸ√x", width = 4, height = 2, bg = 'LightSteelBlue', font = ("Helvetica", 10, "bold"), command = lambda: self.small_interface_shift('yroot'))
+		one_over = Button(master, text = "1/x", width = 4, height = 2, bg = 'LightSteelBlue', font = ("Helvetica", 10, "bold"), command = lambda: self.press_exponentiate(-1))
+		square.grid(column = 5, row = 2, padx = 3, pady = 3)
 		exponent.grid(column = 4, row = 3, padx = 3, pady = 3)
-		squareroot = Button(master, text = "√x", width = 4, height = 2, bg = 'light blue', font = ("Helvetica", 10, "bold"))
-		squareroot.grid(column = 4, row = 4, padx = 3, pady = 3)
-		some_root = Button(master, text = "ʸ√x", width = 4, height = 2, bg = 'light blue', font = ("Helvetica", 10, "bold"), command = lambda: self.small_interface_shift('yroot'))
-		some_root.grid(column = 4, row = 5, padx = 3, pady = 3)
-		one_over = Button(master, text = "1/x", width = 4, height = 2, bg = 'light blue', font = ("Helvetica", 10, "bold"))
-		one_over.grid(column = 4, row = 6, padx = 3, pady = 3)
-
+		squareroot.grid(column = 4, row = 2, padx = 3, pady = 3)
+		some_root.grid(column = 5, row = 3, padx = 3, pady = 3)
+		one_over.grid(column = 4, row = 4, padx = 3, pady = 3)
+		square.bind("<Enter>", lambda event: square.config(bg = 'gold'))
+		exponent.bind("<Enter>", lambda event: exponent.config(bg = 'gold'))
+		squareroot.bind("<Enter>", lambda event: squareroot.config(bg = 'gold'))
+		some_root.bind("<Enter>", lambda event: some_root.config(bg = 'gold'))
+		one_over.bind("<Enter>", lambda event: one_over.config(bg = 'gold'))
+		square.bind("<Leave>", lambda event: square.config(bg = "LightSteelBlue"))
+		exponent.bind("<Leave>", lambda event: exponent.config(bg = "LightSteelBlue"))
+		squareroot.bind("<Leave>", lambda event: squareroot.config(bg = "LightSteelBlue"))
+		some_root.bind("<Leave>", lambda event: some_root.config(bg = "LightSteelBlue"))
+		one_over.bind("<Leave>", lambda event: one_over.config(bg = "LightSteelBlue"))
 
 	def show_number(self, number, *args):
 		if len(self.display_numbers.get()) < self.display_maxLength:
@@ -210,15 +224,25 @@ class Interface:
 
 			self.clean_up(answer)
 
+	def press_exponentiate(self, b, *args):
+		if len(self.display_numbers.get()):
+			answer = str(self.Calc.exponentiate(float(self.display_numbers.get()), b))
+			self.clean_up(answer)
+
+	def make_negative(self):
+		if len(self.display_numbers.get()):
+			temp = self.display_numbers.get()
+			if int(temp) == float(temp):
+				self.display_numbers.set(str(int(temp) * -1))
+			else:
+				self.display_numbers.set(str(float(temp) * -1))
+
 	def clean_up(self, answer):
 		self.clear_interface()
 		if answer[-2:] == '.0':
 			answer = answer[:-2]
 		self.display_numbers.set(answer)
 
-	def press_one_over(self):
-	
-		self.Calc
 
 GUI = Interface(root)
 root.mainloop()
